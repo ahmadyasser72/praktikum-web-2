ea576c4b50bfe6f07396ec4a7e3e05a8d0be813d
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-05-14 16:58:01 +0800

    SESI 3 - 4
    Ubah Data Karyawan

--- pages/karyawan/karyawan.php
+++ pages/karyawan/karyawan.php
@@ -53,11 +53,11 @@ include "database/connection.php";
           <td> <?php echo $row["nama"] ?> </td>
           <td> <?php echo $row["nama_bagian"] ?> </td>
           <td>
-            <a href="?page=karyawanubah&id=<?php echo $row['nik'] ?>" class="btn btn-primary">
+            <a href="?page=karyawanubah&nik=<?php echo $row['nik'] ?>" class="btn btn-primary">
               <i class="fa fa-edit"></i>
               Ubah
             </a>
-            <a href="?page=karyawanhapus&id=<?php echo $row['nik'] ?>"
+            <a href="?page=karyawanhapus&nik=<?php echo $row['nik'] ?>"
                onclick="javascript: return confirm('Konfirmasi data akan dihapus?')"
                class="btn btn-danger">
               <i class="fa fa-trash"></i>
