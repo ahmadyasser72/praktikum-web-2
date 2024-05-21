590bccb902cffb5d87bcf2a2c5e8b81abc412efc
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-05-21 17:25:31 +0800

    SESI 4 - 4
    Bonus Sidebar

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,3 +1,44 @@
+<?php
+$dashboard_active = "fw-bold";
+$bagian_active = "fw-bold";
+$karyawan_active = "fw-bold";
+$penggajian_active = "fw-bold";
+
+if (isset($_GET['page'])) {
+  $page = $_GET['page'];
+} else {
+  $page = '';
+}
+
+switch ($page) {
+  case "":
+  case "dashboard":
+    $dashboard_active = "active";
+    break;
+  case "bagian":
+  case "bagiantambah":
+  case "bagianubah":
+  case "bagianhapus":
+    $bagian_active = "active";
+    break;
+  case "karyawan":
+  case "karyawantambah":
+  case "karyawanubah":
+  case "karyawanhapus":
+    $karyawan_active = "active";
+    break;
+  case "penggajian":
+  case "penggajiantambah":
+  case "penggajianhapus":
+  case "pilihbulantahunpenggajian":
+  case "pilihkaryawanpenggajian":
+    $penggajian_active = "active";
+    break;
+  default:
+    $dash_board_active = "active";
+}
+?>
+
 <div class="bg-white" id="sidebar-wrapper">
   <div
     class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"
@@ -7,22 +48,22 @@
   <div class="list-group list-group-flush my-3">
     <a
       href="?page=dashboard"
-      class="list-group-item list-group-item-action bg-transparent second-text active"
+      class="list-group-item list-group-item-action bg-transparent second-text <?= $dashboard_active ?>"
       ><i class="fas fa-tachometer-alt me-2"></i>Dashboard</a
     >
     <a
       href="?page=bagian"
-      class="list-group-item list-group-item-action bg-transparent second-text fw-bold"
+      class="list-group-item list-group-item-action bg-transparent second-text <?= $bagian_active ?>"
       ><i class="fas fa-building me-2"></i>Bagian</a
     >
     <a
       href="?page=karyawan"
-      class="list-group-item list-group-item-action bg-transparent second-text fw-bold"
+      class="list-group-item list-group-item-action bg-transparent second-text <?= $karyawan_active ?>"
       ><i class="fas fa-users me-2"></i>Karyawan</a
     >
     <a
       href="?page=pilihbulantahunpenggajian"
-      class="list-group-item list-group-item-action bg-transparent second-text fw-bold"
+      class="list-group-item list-group-item-action bg-transparent second-text <?= $penggajian_active ?>"
       ><i class="fas fa-money-bill me-2"></i>Penggajian</a
     >
     <a
